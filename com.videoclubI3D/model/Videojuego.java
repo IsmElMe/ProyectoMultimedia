305ed3484ea23188c4fb7238d4bb960c0ef0187e
IsmElMe <ismelemed@alu.edu.gva.es>
2023-03-30 16:59:08 +0200

    Base clase Videojuego

--- com.videoclubI3D/model/Videojuego.java
+++ com.videoclubI3D/model/Videojuego.java
@@ -1,4 +1,23 @@
 package model;
 
 public class Videojuego extends Multimedia {
+    private Plataforma[] plataformas;
+
+    public Videojuego(String titulo, String autor, String formato, String anio, Plataforma[] plataformas) {
+        super(titulo, autor, formato, anio);
+        setPlataformas(plataformas);
+    }
+
+    @Override
+        public String toString(){
+            return super.toString() +
+                    "\nPlataformas: " + getPlataformas();
+        }
+    public Plataforma[] getPlataformas() {
+        return plataformas;
+    }
+
+    public void setPlataformas(Plataforma[] plataformas) {
+        this.plataformas = plataformas;
+    }
 }
