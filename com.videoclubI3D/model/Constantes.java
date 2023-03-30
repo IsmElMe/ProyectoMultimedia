24e86bd2a4ba1bdec6330f4a70f9383b2c83a0b7
Excalibur783 <ismaelruiziniguez@gmail.com>
2023-03-30 16:35:19 +0200

    Atributos clase Multimedia y clase Constantes

---
+++ com.videoclubI3D/model/Constantes.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Constantes {
+    public static final String TITULO_DEFECTO = "";
+    public static final String AUTOR_DEFECTO = "";
+    public static final int ANIO_DEFETO = 1980;
+
+}
