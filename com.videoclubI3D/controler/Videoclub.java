d429ec732ea68552e7f895a1233f8600c31cf861
IsmElMe <ismelemed@alu.edu.gva.es>
2023-04-01 16:15:15 +0200

    Creación del método altaSocio y añadido ArrayList de Multimedia a Socio

--- com.videoclubI3D/controler/Videoclub.java
+++ com.videoclubI3D/controler/Videoclub.java
@@ -1,10 +1,17 @@
 package controler;
 
+import model.Multimedia;
+import model.Socio;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Videoclub {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<Socio> socios = new ArrayList<>();
+        ArrayList<Multimedia> multimedias = new ArrayList<>();
         int opcionAltas = 0;
         System.out.println("""
                 Altas:
@@ -16,15 +23,39 @@ public class Videoclub {
                 """);
         opcionAltas = sc.nextInt();
 
-        switch (opcionAltas){
+        switch (opcionAltas) {
             case 1:
+                socios.add(altaSocio(sc));
                 break;
-            case 2:
-                break;
-            case 3:
-                break;
+
+            case 2: break;
+
+            case 3: break;
+
         }
     }
 
+    public static Socio altaSocio(Scanner sc) {
+        byte mesSocio, diaSocio;
+        int anioSocio;
+        String nif, nombre, poblacion;
 
+        System.out.println("Introduce el nif del socio");
+        nif = sc.nextLine();
+        System.out.println("Introduce el nombre del socio");
+        nombre = sc.nextLine();
+        System.out.println("Introduce el dia de nacimiento del socio");
+        diaSocio = sc.nextByte();
+        sc.nextLine();
+        System.out.println("Introduce el mes de nacimiento del socio");
+        mesSocio = sc.nextByte();
+        sc.nextLine();
+        System.out.println("Introduce el año de nacimiento del socio");
+        anioSocio = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Introduce la población del socio");
+        poblacion = sc.nextLine();
+
+        return new Socio(LocalDate.of(anioSocio, mesSocio, diaSocio), nombre, poblacion, nif);
+    }
 }
