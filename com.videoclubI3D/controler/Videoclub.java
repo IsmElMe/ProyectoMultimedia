347407db6b31b468e11bf55e8c151bc047992492
Benawam <ismaelauam@gmail.com>
2023-04-01 16:07:20 +0200

    método altaPeliculas

--- com.videoclubI3D/controler/Videoclub.java
+++ com.videoclubI3D/controler/Videoclub.java
@@ -1,10 +1,20 @@
 package controler;
 
+import model.Formato;
+import model.Multimedia;
+import model.Pelicula;
+import model.Socio;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Videoclub {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        ArrayList<Socio> socios = new ArrayList<>();
+        ArrayList<Multimedia> multimedias = new ArrayList<Multimedia>();
+
         int opcionAltas = 0;
         System.out.println("""
                 Altas:
@@ -20,11 +30,30 @@ public class Videoclub {
             case 1:
                 break;
             case 2:
+                altasPeliculas(sc, multimedias, socios);
                 break;
             case 3:
                 break;
         }
     }
 
+public static void altasPeliculas(Scanner sc, ArrayList<Multimedia> multimedias, ArrayList<Socio> socios){
+    System.out.println("Introduce el título de la película: ");
+    String titulo = sc.nextLine();
+    System.out.println("Introduce el nombre del director de la película: ");
+    String autor = sc.nextLine();
+    System.out.println("Introduce el formato de la película: ");
+    Formato formato = Formato.CD;
+    System.out.println("Introduce el anio de la película: ");
+    int anio = sc.nextInt();
+    System.out.println("Introduce la duración de la película: ");
+    int duracion = sc.nextInt();
+    System.out.println("Introduce el nombre del actor principal: ");
+    String actorPrincipal = sc.nextLine();
+    System.out.println("Introduce el nombre de la actriz principal: ");
+    String actrizPrinciapl = sc.nextLine();
+
+    multimedias.add(new Pelicula(titulo, autor, formato, anio, duracion, actorPrincipal, actrizPrinciapl));
+}
 
 }
