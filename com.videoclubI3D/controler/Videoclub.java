efbc2c5bfb4e8099eea0db5cefab51dfde89adad
Excalibur783 <ismaelruiziniguez@gmail.com>
2023-04-01 15:16:52 +0200

    Switch en el main

--- com.videoclubI3D/controler/Videoclub.java
+++ com.videoclubI3D/controler/Videoclub.java
@@ -1,4 +1,30 @@
 package controler;
 
+import java.util.Scanner;
+
 public class Videoclub {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcionAltas = 0;
+        System.out.println("""
+                Altas:
+                1 Alta de Socio
+                2 Alta de película
+                3 Alta de videojuego
+                4 Alta de disco
+                0 Salir
+                """);
+        opcionAltas = sc.nextInt();
+
+        switch (opcionAltas){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+    }
+
+
 }
