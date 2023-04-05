79537a852003775834251a71cbc4b025123869e7
IsmElMe <ismelemed@alu.edu.gva.es>
2023-04-05 20:01:34 +0200

    Condición altaSocio mayores de edad

--- com.videoclubI3D/model/Pelicula.java
+++ com.videoclubI3D/model/Pelicula.java
@@ -3,61 +3,61 @@ package model;
 import java.time.LocalDate;
 
 public class Pelicula extends Multimedia {
-    private int duracion;
-    private String actorPrincipal;
-    private String actrizPrinciapl;
+	private int duracion;
+	private String actorPrincipal;
+	private String actrizPrinciapl;
 
-    public Pelicula(){
-        super();
-        duracion = 0;
-        actorPrincipal = "";
-        actrizPrinciapl = "";
-    }
+	public Pelicula() {
+		super();
+		duracion = Constantes.DURACION_INT_DEFAULT;
+		actorPrincipal = Constantes.AUTOR_DEFECTO;
+		actrizPrinciapl = Constantes.AUTOR_DEFECTO;
+	}
 
-    public Pelicula(String titulo, String autor, Formato formato, int anio, int duracion, String actorPrincipal, String actrizPrinciapl) {
-        super(titulo, autor, formato, anio);
-        setDuracion(duracion);
-        setActorPrincipal(actorPrincipal);
-        setActrizPrinciapl(actrizPrinciapl);
+	public Pelicula(String titulo, String autor, Formato formato, int anio, int duracion, String actorPrincipal, String actrizPrinciapl) {
+		super(titulo, autor, formato, anio);
+		setDuracion(duracion);
+		setActorPrincipal(actorPrincipal);
+		setActrizPrinciapl(actrizPrinciapl);
 
-        if(anio < 2012){
-            setPrecio(Constantes.PRECIO_DEFAULT - 1);
-        }
+		if (anio < 2012) {
+			setPrecio(Constantes.PRECIO_DEFAULT - 1);
+		}
 
-        if (anio == LocalDate.now().getYear()){
-            setPrecio(Constantes.PRECIO_DEFAULT + 1);
-        }
-    }
+		if (anio == LocalDate.now().getYear()) {
+			setPrecio(Constantes.PRECIO_DEFAULT + 1);
+		}
+	}
 
-    @Override
-    public String toString() {
-        return super.toString() +
-                "\nDuración: " + getDuracion() +
-                "\nActor Principal: " + getActorPrincipal() +
-                "\nActriz Principal: " + getActrizPrinciapl();
-    }
+	public int getDuracion() {
+		return duracion;
+	}
 
-    public int getDuracion() {
-        return duracion;
-    }
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
 
-    public void setDuracion(int duracion) {
-        this.duracion = duracion;
-    }
+	public String getActorPrincipal() {
+		return actorPrincipal;
+	}
 
-    public String getActorPrincipal() {
-        return actorPrincipal;
-    }
+	public void setActorPrincipal(String actorPrincipal) {
+		this.actorPrincipal = actorPrincipal;
+	}
 
-    public void setActorPrincipal(String actorPrincipal) {
-        this.actorPrincipal = actorPrincipal;
-    }
+	public String getActrizPrinciapl() {
+		return actrizPrinciapl;
+	}
 
-    public String getActrizPrinciapl() {
-        return actrizPrinciapl;
-    }
+	public void setActrizPrinciapl(String actrizPrinciapl) {
+		this.actrizPrinciapl = actrizPrinciapl;
+	}
 
-    public void setActrizPrinciapl(String actrizPrinciapl) {
-        this.actrizPrinciapl = actrizPrinciapl;
-    }
+	@Override
+	public String toString() {
+		return super.toString() +
+				"\nDuración: " + getDuracion() +
+				"\nActor Principal: " + getActorPrincipal() +
+				"\nActriz Principal: " + getActrizPrinciapl();
+	}
 }
--- com.videoclubI3D/model/Socio.java
+++ com.videoclubI3D/model/Socio.java
@@ -5,82 +5,82 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class Socio {
-    private static int numSocio;
-    private final String NIF;
-    private String nombre, poblacion;
-    private LocalDate fechaNacimiento;
-    private ArrayList<Multimedia> multimediasAlquiladas;
+	private static int numSocio;
+	private final String NIF;
+	private String nombre, poblacion;
+	private LocalDate fechaNacimiento;
+	private ArrayList<Multimedia> multimediasAlquiladas;
 
-    public Socio(LocalDate fechaNacimiento, String nombre, String poblacion, String NIF) {
-        numSocio++;
-        this.fechaNacimiento = fechaNacimiento;
-        this.nombre = nombre;
-        this.poblacion = poblacion;
-        this.NIF = NIF;
-        multimediasAlquiladas = new ArrayList<>();
-    }
+	public Socio(LocalDate fechaNacimiento, String nombre, String poblacion, String NIF) {
+		numSocio++;
+		this.fechaNacimiento = fechaNacimiento;
+		this.nombre = nombre;
+		this.poblacion = poblacion;
+		this.NIF = NIF;
+		multimediasAlquiladas = new ArrayList<>();
+	}
 
-    public static int getNumSocio() {
-        return numSocio;
-    }
+	public static int getNumSocio() {
+		return numSocio;
+	}
 
-    public LocalDate getFechaNacimiento() {
-        return fechaNacimiento;
-    }
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
 
-    public void setFechaNacimiento(LocalDate fechaNacimiento) {
-        this.fechaNacimiento = fechaNacimiento;
-    }
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    public String getPoblacion() {
-        return poblacion;
-    }
+	public String getPoblacion() {
+		return poblacion;
+	}
 
-    public void setPoblacion(String poblacion) {
-        this.poblacion = poblacion;
-    }
+	public void setPoblacion(String poblacion) {
+		this.poblacion = poblacion;
+	}
 
-    public String getNIF() {
-        return NIF;
-    }
+	public String getNIF() {
+		return NIF;
+	}
 
-    public ArrayList<Multimedia> getMultimediasAlquiladas() {
-        return multimediasAlquiladas;
-    }
+	public ArrayList<Multimedia> getMultimediasAlquiladas() {
+		return multimediasAlquiladas;
+	}
 
-    public void setMultimediasAlquiladas(ArrayList<Multimedia> multimediasAlquiladas) {
-        this.multimediasAlquiladas = multimediasAlquiladas;
-    }
+	public void setMultimediasAlquiladas(ArrayList<Multimedia> multimediasAlquiladas) {
+		this.multimediasAlquiladas = multimediasAlquiladas;
+	}
 
-    private boolean comprobarNif(String nif) {
-        int dniNum = Integer.parseInt(nif.substring(0, 8)) % 23;
-        String letra = "TRWAGMYFPDXBNJZSQVHLCKE";
+	private boolean comprobarNif(String nif) {
+		int dniNum = Integer.parseInt(nif.substring(0, 8)) % 23;
+		String letra = "TRWAGMYFPDXBNJZSQVHLCKE";
 
-        return nif.equals(nif.substring(0, 8) + letra.charAt(dniNum));
-    }
+		return nif.equals(nif.substring(0, 8) + letra.charAt(dniNum));
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof Socio)
-            return NIF.equals(((Socio) obj).NIF);
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Socio)
+			return NIF.equals(((Socio) obj).NIF);
 
-        return false;
-    }
+		return false;
+	}
 
-    @Override
-    public String toString() {
-        return "Socio n#" + numSocio +
-                "\n \tNIF: " + NIF +
-                "\n \tNombre: " + nombre +
-                "\n \tFecha nacimiento: " + fechaNacimiento.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) +
-                "\n \tPoblación: " + poblacion;
-    }
+	@Override
+	public String toString() {
+		return "Socio n#" + numSocio +
+				"\n \tNIF: " + NIF +
+				"\n \tNombre: " + nombre +
+				"\n \tFecha nacimiento: " + fechaNacimiento.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) +
+				"\n \tPoblación: " + poblacion;
+	}
 }
