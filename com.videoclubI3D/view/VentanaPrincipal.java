c57524646cd80303b5a4aaa036c065d1ac2f62e0
IsmElMe <ismelemed@alu.edu.gva.es>
2023-04-27 15:55:25 +0200

    Base de la ventana alta pelicula

--- com.videoclubI3D/view/VentanaPrincipal.java
+++ com.videoclubI3D/view/VentanaPrincipal.java
@@ -1,5 +1,7 @@
 package view;
 
+import model.Constantes;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -15,34 +17,42 @@ public class VentanaPrincipal extends JFrame {
         super.setContentPane(panel);
         super.setLayout(null);
         super.setResizable(false);
-        super.setSize(600, 500);
+        super.setBounds((Constantes.ANCHO_PANTALLA / 2) - 300, (Constantes.ALTO_PANTALLA / 2) - 250, 600, 500);
         super.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        // Width y Font se pondrá en constantes más tarde
+
+        Rectangle rectangle = new Rectangle((super.getWidth() / 2) - 175, 140, 350, 40);
         lblTitulo = new JLabel("Videoclub I3D");
         panel.add(lblTitulo);
-        lblTitulo.setFont(new Font("Titulo", Font.BOLD, 30));
+        lblTitulo.setFont(Constantes.FUENTE_TITULO);
         lblTitulo.setBounds((super.getWidth() / 2) - 100, 15, 200, 100);
 
         btnAltas = new JButton("Altas");
         panel.add(btnAltas);
-        btnAltas.setFont(new Font("Titulo", Font.BOLD, 25));
-        btnAltas.setBounds((super.getWidth() / 2) - 150, 140, 300, 40);
+        btnAltas.setFont(Constantes.FUENTE_BOTON);
+        btnAltas.setBounds(rectangle);
+        rectangle.y += 80;
 
         btnAlquilarMultimedaSocio = new JButton("Alquilar multimedia a socio");
         panel.add(btnAlquilarMultimedaSocio);
-        btnAlquilarMultimedaSocio.setFont(new Font("Titulo", Font.BOLD, 20));
-        btnAlquilarMultimedaSocio.setBounds((super.getWidth() / 2) - 150, 220, 300, 40);
+        btnAlquilarMultimedaSocio.setFont(Constantes.FUENTE_BOTON);
+        btnAlquilarMultimedaSocio.setBounds(rectangle);
+        rectangle.y += 80;
 
         btnDevolverMultimedia = new JButton("Devolver multimedia de socio");
         panel.add(btnDevolverMultimedia);
-        btnDevolverMultimedia.setFont(new Font("Titulo", Font.BOLD, 15));
-        btnDevolverMultimedia.setBounds((super.getWidth() / 2) - 150, 300, 300, 40);
+        btnDevolverMultimedia.setFont(Constantes.FUENTE_BOTON);
+        btnDevolverMultimedia.setBounds(rectangle);
+        rectangle.y += 80;
 
         btnListar = new JButton("Listar");
         panel.add(btnListar);
-        btnListar.setFont(new Font("Titulo", Font.BOLD, 30));
-        btnListar.setBounds((super.getWidth() / 2) - 150, 380, 300, 40);
+        btnListar.setFont(Constantes.FUENTE_BOTON);
+        btnListar.setBounds(rectangle);
 
         super.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new VentanaPrincipal();
     }
 }
