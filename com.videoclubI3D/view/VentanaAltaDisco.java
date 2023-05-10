e857c37fd8e0560ae1fb82a735ea7815fc319a5c
Excalibur783 <ismaelruiziniguez@gmail.com>
2023-05-10 17:49:55 +0200

    VentanaAltaDisco

--- com.videoclubI3D/view/VentanaAltaDisco.java
+++ com.videoclubI3D/view/VentanaAltaDisco.java
@@ -26,11 +26,25 @@ public class VentanaAltaDisco extends JFrame {
         super("Blockbuster - Alta Disco");
         panel = new JPanel();
         super.setContentPane(panel);
+        super.setLayout(null);
         super.setDefaultCloseOperation(HIDE_ON_CLOSE);
         super.setResizable(false);
         super.setBounds((Constantes.ANCHO_PANTALLA / 2) - 250, (Constantes.ALTO_PANTALLA / 2) - 235, 500, 470);
 
         crearLabel();
+        crearInput();
+
+        btnGuardar = new JButton("Guardar");
+        panel.add(btnGuardar);
+        btnGuardar.setBounds(super.getWidth() - 150, super.getHeight() - 70, 100, 20);
+
+        btnAnyadir = new JButton("Añadir");
+        panel.add(btnAnyadir);
+        btnAnyadir.setBounds(super.getWidth() -140, 270, 100, 20);
+
+        btnEliminar = new JButton("Eliminar");
+        panel.add(btnEliminar);
+        btnEliminar.setBounds(super.getWidth() -140, 300, 100, 20);
     }
 
     public void crearLabel(){
@@ -70,7 +84,7 @@ public class VentanaAltaDisco extends JFrame {
         panel.add(lblFormato);
         lblFormato.setFont(Constantes.FUENTE_LABEL);
         lblFormato.setBounds(rectangleLabel);
-        rectangleLabel.y += 40;
+        rectangleLabel.x += 201;
 
         lblCanciones = new JLabel("Canciones:");
         panel.add(lblCanciones);
@@ -92,7 +106,8 @@ public class VentanaAltaDisco extends JFrame {
 
         spnDuracion = new JSpinner();
         panel.add(spnDuracion);
-        spnDuracion.setModel(new SpinnerNumberModel(60, 60, 240, 1));
+        spnDuracion.setEnabled(false);
+        spnDuracion.setModel(new SpinnerNumberModel(20, 20,240, 1));
         spnDuracion.setBounds(130, 180, 50, 20);
 
         cbAnyo = new JComboBox<>();
@@ -108,5 +123,45 @@ public class VentanaAltaDisco extends JFrame {
         rectangleTextField.width -= 60;
         rectangleTextField.x += 60;
         rectangleTextField.y += 80;
+
+        Rectangle rectangleRadioButton = new Rectangle(100, 250, 100,20);
+        bgFormato = new ButtonGroup();
+        rbCd = new JRadioButton("CD");
+        rbDvd = new JRadioButton("DVD");
+        rbBluray = new JRadioButton("BLUE-RAY");
+        rbArchivo = new JRadioButton("Archivo");
+        bgFormato.add(rbCd);
+        bgFormato.add(rbDvd);
+        bgFormato.add(rbBluray);
+        bgFormato.add(rbArchivo);
+        panel.add(rbCd);
+        panel.add(rbDvd);
+        panel.add(rbBluray);
+        panel.add(rbArchivo);
+        rbCd.setSelected(true);
+
+        rbCd.setBounds(rectangleRadioButton);
+        rectangleRadioButton.y += 20;
+        rbDvd.setBounds(rectangleRadioButton);
+        rectangleRadioButton.y += 20;
+        rbBluray.setBounds(rectangleRadioButton);
+        rectangleRadioButton.y += 20;
+        rbArchivo.setBounds(rectangleRadioButton);
+
+        cbCanciones = new JComboBox<>();
+        panel.add(cbCanciones);
+        cbCanciones.setEditable(true);
+        cbCanciones.setBounds(super.getWidth() - 150, 220, 100, 20);
+
+        taCancionesDisco = new JTextArea();
+        panel.add(taCancionesDisco);
+        taCancionesDisco.setEditable(false);
+        taCancionesDisco.setBounds(super.getWidth() - 250, 260, 100, 80);
+    }
+
+    public static void main(String[] args) {
+        VentanaAltaDisco gui = new VentanaAltaDisco();
+        gui.setVisible(true);
+        gui.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
